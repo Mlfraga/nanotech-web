7195708f34ea137a067ddcc3dea8973f76dfefb6
Matheus Fraga <matheuslf44@gmail.com>
2021-08-26 00:28:37 -0300

    chore: Add build

---
+++ build/precache-manifest.d23da2209a7bd64a8ec811e20bd6bc24.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c224fa6605efe4bfdcf5821a1c2e6020",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdbf5bcd77e649450de8",
+    "url": "/static/css/2.c01a9049.chunk.css"
+  },
+  {
+    "revision": "350e7f3f97f569c32952",
+    "url": "/static/css/main.744b2262.chunk.css"
+  },
+  {
+    "revision": "cdbf5bcd77e649450de8",
+    "url": "/static/js/2.d975b55d.chunk.js"
+  },
+  {
+    "revision": "50d0b0ddc287ebcc4a1ab1e220ad1ef0",
+    "url": "/static/js/2.d975b55d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "350e7f3f97f569c32952",
+    "url": "/static/js/main.cf865821.chunk.js"
+  },
+  {
+    "revision": "3bf522de120c62cc05ce",
+    "url": "/static/js/runtime-main.4af6b794.js"
+  },
+  {
+    "revision": "7a3dbad3c9ab60180ba203b9debe2f1a",
+    "url": "/static/media/Logo.7a3dbad3.png"
+  },
+  {
+    "revision": "b3db49cee06d7bf3e4350e2227490711",
+    "url": "/static/media/Pattern-Developing-Page.b3db49ce.svg"
+  },
+  {
+    "revision": "bc0745b0573047573dbf27bcfecce831",
+    "url": "/static/media/SmallLogo.bc0745b0.png"
+  },
+  {
+    "revision": "4fccbeac8381e5b9a88f481d6ebbc113",
+    "url": "/static/media/sign-in-background-3.4fccbeac.jpg"
+  },
+  {
+    "revision": "c56c732a75920cf635ed7614b01897b9",
+    "url": "/static/media/sign-in-background-6.c56c732a.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.70afabdd8cfabd769e16c91e202cac8e.js"
+  "/precache-manifest.d23da2209a7bd64a8ec811e20bd6bc24.js"
 );
 
 self.addEventListener('message', (event) => {
