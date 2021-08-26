abd0441ece4e24434d9d790a0b254a3fc4fa8b2f
Matheus Fraga <matheuslf44@gmail.com>
2021-08-25 22:02:03 -0300

    chore: Add build

---
+++ build/precache-manifest.70afabdd8cfabd769e16c91e202cac8e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d63137a4eaef7c995a00a87bfd4117",
+    "url": "/index.html"
+  },
+  {
+    "revision": "054e20fa7a5e819f1f3b",
+    "url": "/static/css/2.c01a9049.chunk.css"
+  },
+  {
+    "revision": "0d65512f19538dba79ae",
+    "url": "/static/css/main.744b2262.chunk.css"
+  },
+  {
+    "revision": "054e20fa7a5e819f1f3b",
+    "url": "/static/js/2.dfe235cb.chunk.js"
+  },
+  {
+    "revision": "50d0b0ddc287ebcc4a1ab1e220ad1ef0",
+    "url": "/static/js/2.dfe235cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d65512f19538dba79ae",
+    "url": "/static/js/main.2cbadb37.chunk.js"
+  },
+  {
+    "revision": "3bf522de120c62cc05ce",
+    "url": "/static/js/runtime-main.4af6b794.js"
+  },
+  {
+    "revision": "7a3dbad3c9ab60180ba203b9debe2f1a",
+    "url": "/static/media/Logo.7a3dbad3.png"
+  },
+  {
+    "revision": "b3db49cee06d7bf3e4350e2227490711",
+    "url": "/static/media/Pattern-Developing-Page.b3db49ce.svg"
+  },
+  {
+    "revision": "bc0745b0573047573dbf27bcfecce831",
+    "url": "/static/media/SmallLogo.bc0745b0.png"
+  },
+  {
+    "revision": "4fccbeac8381e5b9a88f481d6ebbc113",
+    "url": "/static/media/sign-in-background-3.4fccbeac.jpg"
+  },
+  {
+    "revision": "c56c732a75920cf635ed7614b01897b9",
+    "url": "/static/media/sign-in-background-6.c56c732a.jpg"
+  }
+]);
